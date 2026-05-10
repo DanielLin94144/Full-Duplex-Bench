890bba6b2ca5337c56888c84af8f887605c13675
Alex Liu <alexhliu@thinkingmachines.ai>
2026-05-10 08:06:29 +0000

    drop trust_repo

--- v1_v1.5/evaluation/eval_general_before_after.py
+++ v1_v1.5/evaluation/eval_general_before_after.py
@@ -78,7 +78,6 @@ def _load_silero(force_reload: bool = False):
         model="silero_vad",
         force_reload=force_reload,
         onnx=False,
-        trust_repo=True,
     )
     return _silero_model, _silero_utils
 
